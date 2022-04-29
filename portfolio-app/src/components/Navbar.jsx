2e4a4e65265f4e7aea6b6e38329f16d96d99fb71
Ivan Sanchez Leyva <deepman42@protonmail.com>
2022-04-29 14:53:04 +0200

    importing logo and icons

--- portfolio-app/src/components/Navbar.jsx
+++ portfolio-app/src/components/Navbar.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import logo from './logo.png'
+import {FaBars, FaTimes} from 'react-icons/fa'
 
 const Navbar = () => {
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f]'>
-
+      
     </div>
   )
 }
