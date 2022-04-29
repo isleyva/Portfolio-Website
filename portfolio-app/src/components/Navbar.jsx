e7a8887cd8c5b2db2b36c065a8162361f3e10039
Ivan Sanchez Leyva <deepman42@protonmail.com>
2022-04-29 15:48:11 +0200

    logo adjust

--- portfolio-app/src/components/Navbar.jsx
+++ portfolio-app/src/components/Navbar.jsx
@@ -4,8 +4,8 @@ import {FaBars, FaTimes} from 'react-icons/fa'
 
 const Navbar = () => {
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f]'>
-      <img src={Logo} alt='Logo image' style={{width: "50 px"}} />
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#434443]'>
+      <img src={Logo} alt='Logo image' style={{width: '200px'}} />
     </div>
   )
 }
