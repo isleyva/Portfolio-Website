ba3d8b3ea5c565696b6fcd3990634b5303355309
Ivan Sanchez Leyva <deepman42@protonmail.com>
2022-04-29 15:31:09 +0200

    implemeinting logo

--- portfolio-app/src/components/Navbar.jsx
+++ portfolio-app/src/components/Navbar.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import logo from './logo.png'
+import Logo from '../assests/logo.png'
 import {FaBars, FaTimes} from 'react-icons/fa'
 
 const Navbar = () => {
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f]'>
-      
+      <img src={Logo} alt='Logo image' style={{width: "50 px"}} />
     </div>
   )
 }
