05c773def6ba9decbefab39636f728899a575975
Ivan Sanchez Leyva <deepman42@protonmail.com>
2022-07-25 16:52:45 +0200

    p tag

--- components/Main.js
+++ components/Main.js
@@ -11,9 +11,11 @@ const Main = () => {
           <h1 className="py-4 text-gray-800">
             Hi, I'm <span className="text-red-500">Ivan</span>{" "}
           </h1>
-          <h1 className=" py-4 text-gray-800">
-          A Front End Web Developer
-          </h1>
+          <h1 className=" py-1 text-gray-800">A Front End Web Developer</h1>
+          <p className="py-4 text-gray-700 max-w-[70%] m-auto">
+            I'm a Front-End Web Developer specializing in React and Next.js buildings for creating excepcional digitals experiences. Currently I’m focused on building responsive front-end web applications while
+            learning back-end technologies.
+          </p>
         </div>
       </div>
     </div>
