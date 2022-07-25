570b790b310ef2b177e2a4b1e79951c5bd50f98d
Ivan Sanchez Leyva <deepman42@protonmail.com>
2022-07-25 16:29:43 +0200

    h1 styling

--- components/Main.js
+++ components/Main.js
@@ -6,7 +6,7 @@ const Main = () => {
         <div className='max-w-[1240px], w-full h-full mx-auto p-2 flex justify-center items-center' >
             <div>
                 <p className="uppercase  text-sm tracking-widest text-gray-600">Let´s build something together</p>
-                <h1>Hi, I'm Ivan</h1>
+                <h1>Hi, I'm <span className='text-red-500'>Ivan</span> </h1>
             </div>
         </div>
     </div>
