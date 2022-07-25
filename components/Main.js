05a912c7da4d6f0b576de6b36dd7160a26e59152
Ivan Sanchez Leyva <deepman42@protonmail.com>
2022-07-25 16:39:37 +0200

    h1 more styling

--- components/Main.js
+++ components/Main.js
@@ -1,16 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const Main = () => {
   return (
-    <div className='w-full h-screen text-center'>
-        <div className='max-w-[1240px], w-full h-full mx-auto p-2 flex justify-center items-center' >
-            <div>
-                <p className="uppercase  text-sm tracking-widest text-gray-600">Let´s build something together</p>
-                <h1>Hi, I'm <span className='text-red-500'>Ivan</span> </h1>
-            </div>
+    <div className="w-full h-screen text-center">
+      <div className="max-w-[1240px], w-full h-full mx-auto p-2 flex justify-center items-center">
+        <div>
+          <p className="uppercase  text-sm tracking-widest text-gray-600">
+            Let´s build something together
+          </p>
+          <h1 className="py-4 text-gray-800">
+            Hi, I'm <span className="text-red-500">Ivan</span>{" "}
+          </h1>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
