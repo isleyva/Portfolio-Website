6e1227f652e5b5462778bcd1b66ed251353a4e62
Ivan Sanchez Leyva <deepman42@protonmail.com>
2022-07-25 16:23:58 +0200

    p tag in Main component and setting Main component in the app file

---
+++ components/Main.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div className='w-full h-screen text-center'>
+        <div className='max-w-[1240px], w-full h-full mx-auto p-2 flex justify-center items-center' >
+            <div>
+                <p className="uppercase  text-sm tracking-widest text-gray-600">Let´s build something together</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Main
