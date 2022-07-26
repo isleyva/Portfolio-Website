23fe6877e0e30041a54f44a8b358853353b4cdd6
Ivan Sanchez Leyva <deepman42@protonmail.com>
2022-07-26 19:06:54 +0200

    Abaout component p tagç

---
+++ components/About.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='w-full md:h-screen p-2 flex items-center py-16'>
+        <div className='max-w-[1240px] m-auto md:grid grid-cols-3 gap-8'>
+            <div className='col-span-2'>
+            <p> About</p>
+            <h2> Who I Am</h2>
+            <p>// Passionate for building digital experiences</p>
+            <p>
+              Hi, my name is Ivan and I have more than 2 years of experience in the world of informatic.
+              Starting whith Cibersecurity
+            </p>
+            <p></p>
+            </div>
+            <div></div>
+        </div>
+    </div>
+  )
+}
+
+export default About
