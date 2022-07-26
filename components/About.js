61d8bf3ce171f07d1c6eb18d7f2a677af87dd0b8
Ivan Sanchez Leyva <deepman42@protonmail.com>
2022-07-27 00:53:57 +0200

    styling p tag

--- components/About.js
+++ components/About.js
@@ -6,7 +6,7 @@ const About = () => {
     <div className='w-full md:h-screen p-2 flex items-center py-16'>
         <div className='max-w-[1240px] m-auto md:grid grid-cols-3 gap-8'>
             <div className='col-span-2'>
-            <p className='py-4 uppercase'> About</p>
+            <p className='uppercase text-xl tracking-widest text-red-500 font-bold'> About</p>
             <h2> Who I Am</h2>
             <p>// Passionate for building digital experiences</p>
             <p>
