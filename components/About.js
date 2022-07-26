2e2bd8ef367c3285e786c9cba43492e743347c73
Ivan Sanchez Leyva <deepman42@protonmail.com>
2022-07-27 01:23:53 +0200

    syling colour text and size

--- components/About.js
+++ components/About.js
@@ -7,16 +7,16 @@ const About = () => {
         <div className='max-w-[1240px] m-auto md:grid grid-cols-3 gap-8'>
             <div className='col-span-2'>
             <p className='uppercase text-xl tracking-widest text-red-500 font-bold'> About</p>
-            <h2> Who I Am</h2>
-            <p>// Passionate for building digital experiences</p>
-            <p>
+            <h2 className='py-4'> Who I Am</h2>
+            <p className="py-2 text-gray-500">// Passionate for building digital experiences</p>
+            <p className="py-2 text-black text-lg">
               Hi, my name is Ivan and I have more than 2 years of experience in the world of informatic.
-              Started whith Cibersecurity a couple of years ago, i learned alot of systems in specialy about Linux.
-              After this i started to learn about React and starting to build competitives web proyects.
+              Started whith Cibersecurity a couple of years ago, I learned alot of systems in specially about Linux.
+              After this I started to learn about React and starting to build web proyects.
               I'm in continuous learning and starting to make new proyects using different technologies like Next.js,Tailwinds and more cool stuff.
-              And also looking for take a professional career into programming.
+              Also looking for take a professional career into programming.
             </p>
-            <p>Check out some of my lastest proyects</p>
+            <p className="py-2 text-black underline cursor-pointer">Check out some of my lastest proyects</p>
             </div>
             <div>
               <img src="" alt='about' className='w-full h-full'/>
