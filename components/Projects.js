7149663b53939ac7b6c930001cb17aa88f340668
Ivan Sanchez Leyva <deepman42@protonmail.com>
2022-07-31 20:13:12 +0200

    making the container in projects

--- components/Projects.js
+++ components/Projects.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <div className='w-full'>
+    <div className='max-w-[1240px] mx-auto px-2 py-16'>
+
+    </div>
+    </div>
   )
 }
 
