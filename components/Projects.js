f77b62015f3be9dc6d897b816591c3244a00167e
Ivan Sanchez Leyva <deepman42@protonmail.com>
2022-08-02 01:18:40 +0200

    making the projets aonther component

--- components/Projects.js
+++ components/Projects.js
@@ -11,14 +11,14 @@ const Projects = () => {
           Projects
         </p>
         <h2 className="p-4"> What I've Built</h2>
-        <div className="grid md:grid-cols -2 gap-8">
+        <div className="grid md:grid-cols-2 gap-8">
           <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-red-800 to-red-500">
           <Image className="rounded-xl group-hover:opacity-10" src={ cryptoImg} alt="/" />
           <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
-            <h3>Cryptobase</h3>
-            <p> React JS</p>
+            <h3 className="text-2xl text-white tracking-wider text-center">Cryptobase</h3>
+            <p className=" pb-4 pt-2 text-white text-center"> React JS</p>
             <Link href="/">
-               <p>More Info</p>
+               <p className="text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer">More Info</p>
             </Link>
             </div>
          </div>
