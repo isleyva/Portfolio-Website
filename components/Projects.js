7adf14d88c793db1619fc5025d2193054d546276
Ivan Sanchez Leyva <deepman42@protonmail.com>
2022-08-01 16:16:49 +0200

    p tag and 1 with styling

--- components/Projects.js
+++ components/Projects.js
@@ -1,13 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const Projects = () => {
   return (
-    <div className='w-full'>
-    <div className='max-w-[1240px] mx-auto px-2 py-16'>
-
+    <div className="w-full">
+      <div className="max-w-[1240px] mx-auto px-2 py-16">
+        <p className="text-xl tracking-widest uppercase text-red-500">
+          Projects
+        </p>
+        <h2 className="p-4"> What I've Built</h2>
+        <div className="grid md:grid-cols -2 gap-8">
+          <div className="relative flex items-center justify-cneter h-auto w-full shadow-xl shadow-gray-400">
+          </div>
+        </div>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-
 import Navbar from '../components/Navbar'
 import Main from '../components/Main'
 import About from '../components/About'
