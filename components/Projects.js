c0253bd8ac9ddb5e9f7e7f70a2fd06f5e5f6d5b6
Ivan Sanchez Leyva <deepman42@protonmail.com>
2022-08-02 02:08:44 +0200

    final

--- components/Projects.js
+++ components/Projects.js
@@ -3,6 +3,9 @@ import Image from "next/image";
 import cryptoImg from "../public/assets/projects/crypto.jpg";
 import Link from "next/link";
 import ProjectsItem from "./ProjectItem";
+import spacexImg from "../public/assets/projects/spacexApiphoto.jpg";
+
+
 
 const Projects = () => {
   return (
@@ -13,7 +16,8 @@ const Projects = () => {
         </p>
         <h2 className="p-4"> What I've Built</h2>
         <div className="grid md:grid-cols-2 gap-8">
-          <ProjectsItem />
+          <ProjectsItem title="Crypto Base" backgroundImg={cryptoImg} projectUrl=""/>
+          <ProjectsItem title="Spacex Api" backgroundImg={spacexImg} projectUrl=""/>
         </div>
       </div>
     </div>
