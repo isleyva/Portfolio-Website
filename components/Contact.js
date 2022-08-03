0e231aa3d435f4a2df65eda40f6b357cccbe8424
Ivan Sanchez Leyva <deepman42@protonmail.com>
2022-08-03 22:55:58 +0200

    styling,p tag an h2

--- components/Contact.js
+++ components/Contact.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className=' w-full lg:h-screen'>
+        <div className='max-w-[1240px] m-auto px-2 py-16 w-full'>
+            <p className='text-xl tracking-widest uppercase text-red-500'>Contact</p>
+            <h2 className='p-4'>Get in touch</h2>
+
+        </div>
+
+    </div>
   )
 }
 
