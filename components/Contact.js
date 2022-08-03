0eaa4e790c03c28ad6b2070f17807c28cf436b26
Ivan Sanchez Leyva <deepman42@protonmail.com>
2022-08-03 23:00:08 +0200

    grid container and impotingcontact in index

--- components/Contact.js
+++ components/Contact.js
@@ -1,16 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const Contact = () => {
   return (
-    <div className=' w-full lg:h-screen'>
-        <div className='max-w-[1240px] m-auto px-2 py-16 w-full'>
-            <p className='text-xl tracking-widest uppercase text-red-500'>Contact</p>
-            <h2 className='p-4'>Get in touch</h2>
+    <div className=" w-full lg:h-screen">
+      <div className="max-w-[1240px] m-auto px-2 py-16 w-full">
+        <p className="text-xl tracking-widest uppercase text-red-500">
+          Contact
+        </p>
+        <h2 className="p-4">Get in touch</h2>
+        <div className="grid lg:grid-cols-5 gap-8">
 
         </div>
-
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Main from '../components/Main'
 import About from '../components/About'
 import Skills from '../components/Skills'
 import Projects from '../components/Projects'
+import Contact from '../components/Contact'
 
 
 export default function Home() {
