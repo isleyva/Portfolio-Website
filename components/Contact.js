0f4a9839bf82c703ac5d070730a6e1613fd4dedd
Ivan Sanchez Leyva <deepman42@protonmail.com>
2022-08-04 00:00:57 +0200

    styling and image

--- components/Contact.js
+++ components/Contact.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Image from "next/image";
+import contactImg from "../public/assets/contact.jpg";
 
 const Contact = () => {
   return (
@@ -9,10 +11,21 @@ const Contact = () => {
         </p>
         <h2 className="p-4">Get in touch</h2>
         <div className="grid lg:grid-cols-5 gap-8">
-            <div className="cols-span-3 lg:col-spam-2 w-full h-full shadow-xl shadow-gray-400 rounded-xl p-4">
-
+          <div className="cols-span-3 lg:col-spam-2 w-full h-full shadow-xl shadow-gray-400 rounded-xl p-4">
+            {/*left*/}
+            <div className="flex flex-col items-center justify-center">
+              <div className="lg:p-4 h-full">
+                    <div>
+                        <Image 
+                        className="rounded-xl hover:scale-105 ease-in duration-300 "
+                        src={contactImg}
+                        alt="/"
+                        />
+                    </div>
+              </div>
             </div>
-
+            {/*Right*/}
+          </div>
         </div>
       </div>
     </div>
