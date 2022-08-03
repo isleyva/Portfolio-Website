1658cd6672cdeb6c48f23bb701add54765551877
Ivan Sanchez Leyva <deepman42@protonmail.com>
2022-08-04 00:56:14 +0200

    right secction

--- components/Contact.js
+++ components/Contact.js
@@ -4,6 +4,8 @@ import contactImg from "../public/assets/contact.jpg";
 import { FaLinkedinIn, FaGithub } from "react-icons/fa";
 import { AiOutlineMail } from "react-icons/ai";
 import { BsFillPersonLinesFill } from "react-icons/bs";
+import Link from "next/link";
+import {HiOutlineChevronDoubleUp} from "react-icons/hi";
 
 const Contact = () => {
   return (
@@ -51,12 +53,83 @@ const Contact = () => {
                   </div>
                 </div>
               </div>
+             </div>
+             {/*right*/}
+             <div className='col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4'>
+            <div className='p-4'>
+              <form
+                action='https://getform.io/f/08ebcd37-f5b5-45be-8c13-714f011ce060'
+                method='POST'
+                enctype='multipart/form-data'
+              >
+                <div className='grid md:grid-cols-2 gap-4 w-full py-2'>
+                  <div className='flex flex-col'>
+                    <label className='uppercase text-sm py-2'>Name</label>
+                    <input
+                      className='border-2 rounded-lg p-3 flex border-gray-300'
+                      type='text'
+                      name='name'
+                    />
+                  </div>
+                  <div className='flex flex-col'>
+                    <label className='uppercase text-sm py-2'>
+                      Phone Number
+                    </label>
+                    <input
+                      className='border-2 rounded-lg p-3 flex border-gray-300'
+                      type='text'
+                      name='phone'
+                    />
+                  </div>
+                </div>
+                <div className='flex flex-col py-2'>
+                  <label className='uppercase text-sm py-2'>Email</label>
+                  <input
+                    className='border-2 rounded-lg p-3 flex border-gray-300'
+                    type='email'
+                    name='email'
+                  />
+                </div>
+                <div className='flex flex-col py-2'>
+                  <label className='uppercase text-sm py-2'>Subject</label>
+                  <input
+                    className='border-2 rounded-lg p-3 flex border-gray-300'
+                    type='text'
+                    name='subject'
+                  />
+                </div>
+                <div className='flex flex-col py-2'>
+                  <label className='uppercase text-sm py-2'>Message</label>
+                  <textarea
+                    className='border-2 rounded-lg p-3 border-gray-300'
+                    rows='10'
+                    name='message'
+                  ></textarea>
+                </div>
+                <button className='w-full p-4 text-gray-100 mt-4'>
+                  Send Message
+                </button>
+              </form>
             </div>
-            {/*Right*/}
           </div>
         </div>
+        <div className='flex justify-center py-12'>
+          <Link href='/'>
+            <a>
+              <div className='rounded-full shadow-lg shadow-gray-400 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+                <HiOutlineChevronDoubleUp
+                  className='text-[#5651e5]'
+                  size={30}
+                />
+              </div>
+            </a>
+          </Link>
+        </div>
       </div>
-    </div>
+
+          </div>
+        </div>
+    
   );
 };
 
