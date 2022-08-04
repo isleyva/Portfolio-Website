7f1df03770e9c105404c45c479962eb0e3ae49ec
Ivan Sanchez Leyva <deepman42@protonmail.com>
2022-08-04 14:24:37 +0200

    BOTTON TO UP

--- components/Contact.js
+++ components/Contact.js
@@ -4,6 +4,8 @@ import contactImg from "../public/assets/contact.jpg";
 import { FaLinkedinIn, FaGithub } from "react-icons/fa";
 import { AiOutlineMail } from "react-icons/ai";
 import { BsFillPersonLinesFill } from "react-icons/bs";
+import { HiOutlineChevronDoubleUp } from "react-icons/hi";
+import Link from "next/link";
 
 const Contact = () => {
   return (
@@ -85,6 +87,18 @@ const Contact = () => {
                 </form>
           </div>
           </div>
+        </div>
+        <div className='flex justify-center py-12'>
+          <Link href='/'>
+            <a>
+              <div className='rounded-full shadow-lg shadow-gray-400 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+                <HiOutlineChevronDoubleUp
+                  className='text-[#f04242]'
+                  size={30}
+                />
+              </div>
+            </a>
+          </Link>
         </div>
       </div>
     </div>
