0cecfecef72c80878d30f09f0ac20851e7578e3e
Ivan Sanchez Leyva <deepman42@protonmail.com>
2022-08-04 14:25:30 +0200

    contact done

--- components/Contact.js
+++ components/Contact.js
@@ -57,43 +57,61 @@ const Contact = () => {
             {/*right*/}
           </div>
           <div className="col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4">
-          <div className="p-4">
-                <form>
-                  <div className="grid md:grid-cols-2 gap-4 w-full py-2">
-                      <div className="flex flex-col">
-                        <label className="uppercase text-sm py-2">Name</label>
-                        <input className="rounded-lg p-3 border-2 flex border-gray-300" type="text" />
-                     </div>   
-                     <div className="flex flex-col">
-                        <label className="uppercase text-sm py-2">Phone Number</label>
-                        <input className="rounded-lg p-3 border-2 flex border-gray-300" type="text" />
-                     </div>   
+            <div className="p-4">
+              <form>
+                <div className="grid md:grid-cols-2 gap-4 w-full py-2">
+                  <div className="flex flex-col">
+                    <label className="uppercase text-sm py-2">Name</label>
+                    <input
+                      className="rounded-lg p-3 border-2 flex border-gray-300"
+                      type="text"
+                    />
                   </div>
-                  <div className="flex flex-col py-2">
-                    <label className="uppercase text-sm py-2">Email</label>
-                    <input className="rounded-lg p-3 border-2 flex border-gray-300" type="email" />
+                  <div className="flex flex-col">
+                    <label className="uppercase text-sm py-2">
+                      Phone Number
+                    </label>
+                    <input
+                      className="rounded-lg p-3 border-2 flex border-gray-300"
+                      type="text"
+                    />
                   </div>
-                  <div className="flex flex-col py-2">
-                    <label className="uppercase text-sm py-2">Subject</label>
-                    <input className="rounded-lg p-3 border-2 flex border-gray-300" type="text" />
-                  </div>
-                  <div className="flex flex-col py-2">
-                    <label className="uppercase text-sm py-2">Message</label>
-                    <textarea className="rounded-lg p-3 border-2  border-gray-300" rows={10} type="text" />
-                  </div>
-                  <button className='w-full p-4 text-gray-100 mt-4'>
+                </div>
+                <div className="flex flex-col py-2">
+                  <label className="uppercase text-sm py-2">Email</label>
+                  <input
+                    className="rounded-lg p-3 border-2 flex border-gray-300"
+                    type="email"
+                  />
+                </div>
+                <div className="flex flex-col py-2">
+                  <label className="uppercase text-sm py-2">Subject</label>
+                  <input
+                    className="rounded-lg p-3 border-2 flex border-gray-300"
+                    type="text"
+                  />
+                </div>
+                <div className="flex flex-col py-2">
+                  <label className="uppercase text-sm py-2">Message</label>
+                  <textarea
+                    className="rounded-lg p-3 border-2  border-gray-300"
+                    rows={10}
+                    type="text"
+                  />
+                </div>
+                <button className="w-full p-4 text-gray-100 mt-4">
                   Send Message
                 </button>
-                </form>
-          </div>
+              </form>
+            </div>
           </div>
         </div>
-        <div className='flex justify-center py-12'>
-          <Link href='/'>
+        <div className="flex justify-center py-12">
+          <Link href="/">
             <a>
-              <div className='rounded-full shadow-lg shadow-gray-400 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+              <div className="rounded-full shadow-lg shadow-gray-400 p-4 cursor-pointer hover:scale-110 ease-in duration-300">
                 <HiOutlineChevronDoubleUp
-                  className='text-[#f04242]'
+                  className="text-[#f04242]"
                   size={30}
                 />
               </div>
