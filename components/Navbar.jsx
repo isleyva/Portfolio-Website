88bf9b440966d2e5ec0cb68342b8f4f9b15ab3ea
Ivan Sanchez Leyva <deepman42@protonmail.com>
2022-08-07 21:07:43 +0200

    debuging logo

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -30,7 +30,7 @@ const Navbar = () => {
     <div className={shadow ? "fixed w-full h-20 shadow-xl z-[100]" : "fixed w-full h-20 z-[100]"}>
       <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
       <Image
-                src="/../public/assets/logo.png"
+                src="/assets/logo.png"
                 alt="/"
                 className="object-cover h-48 w-96 "
                 width="210"
