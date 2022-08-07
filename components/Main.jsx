ace8219229f93463246f39989b6b5b235c18c87a
Ivan Sanchez Leyva <deepman42@protonmail.com>
2022-08-08 01:03:28 +0200

    removing a

--- components/Main.jsx
+++ components/Main.jsx
@@ -15,7 +15,7 @@ const Main = () => {
           <h1 className="py-4 text-gray-800">
             Hi, I&apos;m <span className="text-red-500">Ivan</span>{" "}
           </h1>
-          <h1 className=" py-1 text-gray-800">A Front End Web Developer</h1>
+          <h1 className=" py-1 text-gray-800"> Front End Web Developer</h1>
           <p className="py-4 text-gray-700 max-w-[70%] m-auto">
             I&apos;m a Front-End Web Developer specializing in React and Next.js buildings for creating excepcional digitals experiences. Currently I&apos;m focused on building responsive front-end web applications while
             learning back-end technologies.
