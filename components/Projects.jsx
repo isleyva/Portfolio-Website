c97f2bcadfb1c0f78f34c5fe200a5087074dc859
Ivan Sanchez Leyva <deepman42@protonmail.com>
2022-08-11 23:00:43 +0200

    app done

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-import Image from "next/image";
 import cryptoImg from "../public/assets/projects/crypto.jpg";
 import Link from "next/link";
 import ProjectsItem from "./ProjectItem";
 import spacexImg from "../public/assets/projects/spacexApiphoto.jpg";
 
-
+const Linkto = () => {
+  ruturn (
+    <Link href="spacex-api-deepman42.vercel.app/"/>
+  )
+} 
 
 const Projects = () => {
   return (
@@ -16,8 +19,8 @@ const Projects = () => {
         </p>
         <h2 className="p-4"> What I&apos;ve Built</h2>
         <div className="grid md:grid-cols-2 gap-8">
-          <ProjectsItem title="Crypto Base" backgroundImg={cryptoImg} projectUrl=""/>
-          <ProjectsItem title="Spacex Api" backgroundImg={spacexImg} projectUrl=""/>
+          <ProjectsItem title="Crypto Base" backgroundImg={cryptoImg} projectUrl="https://crypto-base.vercel.app/"/>
+          <ProjectsItem title="Spacex Api" backgroundImg={spacexImg} projectUrl="https://spacex-api-deepman42.vercel.app/"/>
         </div>
       </div>
     </div>
